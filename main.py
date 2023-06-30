0b556ef2f5f099798a184b083fb17b4bcd74f839
Jeffrey Scott <valenpendragon@gmail.com>
2023-06-30 06:39:55 -0400

    Create QApplication. Complete ChatbotWindow.__init__() method.

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-class ChatbotWindow:
-    pass
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, \
+    QLineEdit, QPushButton
+import sys
+
+
+class ChatbotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(700, 500)
+        self.setWindowTitle("OpenAI Chatbot")
+
+        # Add chat area widget.
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setReadOnly(True)
+
+        # Add input area widget.
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10, 340, 480, 40)
+
+        # Add the bottom widget.
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(500, 340, 100, 40)
+
+        # We need to call this method when we are not using a Central Widget.
+        self.show()
 
 
 class Chatbot:
     pass
+
+
+app = QApplication(sys.argv)
+main_window = ChatbotWindow()
+sys.exit(app.exec())
